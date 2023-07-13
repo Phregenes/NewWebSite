89049c9d68636d3be0bd173c749d116c74c074f7
Pedro Regenes <pedroregenes@MacBook-Air-de-Pedro.local>
2023-07-13 16:05:20 -0300

    add target=_blank

--- src/components/Barcontact/index.jsx
+++ src/components/Barcontact/index.jsx
@@ -5,19 +5,19 @@ const Barcontact = () =>(
   <div className="barcontact">
     <div className="container_ul">
     <div className="container_li">
-      <a href="https://www.linkedin.com/in/phregenes/">
+      <a href="https://www.linkedin.com/in/phregenes/" target="_blank">
         <i className="fa fa-facebook" aria-hidden="true"></i>
         <span>•Linkedin</span>
       </a>
     </div>
     <div className="container_li">
-      <a href="https://github.com/Phregenes">
+      <a href="https://github.com/Phregenes" target="_blank">
         <i className="fa fa-google-plus" aria-hidden="true"></i>
         <span>•GitHub</span>
       </a>
     </div>
     <div className="container_li">
-      <a href="https://instagram.com/phregenes/">
+      <a href="https://instagram.com/phregenes/" target="_blank">
         <i className="fa fa-instagram" aria-hidden="true"></i>
         <span>•Instagram</span>
       </a>
