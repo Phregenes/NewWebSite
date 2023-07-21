d252898a1f8bb4158d110aa1d87d4e5f86343c67
Pedro Regenes <phregenes@gmail.com>
2023-07-21 15:46:32 -0300

    fix: criando arquivo de mock para os dados das skills e utilizando map para ficar mais limpo o codigo

---
+++ src/components/Skills/data.js
@@ -0,0 +1,32 @@
+import Html from "../../assets/img/html.svg";
+import Css from "../../assets/img/css.svg";
+import Js from "../../assets/img/js.svg";
+import ReactLogo from "../../assets/img/react.svg";
+
+export const data = [
+  {
+    id: 1,
+    title: "HTML",
+    description: "O HyperText Markup Language, conhecido como HTML, é a linguagem de marcação padrão para a criação de documentos destinados a serem exibidos em um navegador da web. Ele pode ser complementado por tecnologias como Cascading Style Sheets (CSS) e linguagens descript, como JavaScript.",
+    img: Html,
+    href: "https://pt.wikipedia.org/wiki/HTML"
+  },
+  {
+    id: 2,
+    title: "CSS",
+    description: "Cascading Style Sheets (CSS) é uma linguagem de estilo utilizada para descrever a apresentação de um documento escrito em uma linguagem de marcação, como HTML ou XML (incluindo dialetos XML como SVG, MathML ou XHTML).",
+    img: Css,
+  },
+  {
+    id: 3,
+    title: "js",
+    description: "JavaScript, frequentemente abreviado como JS, é uma linguagem de programação que se destaca como uma das principais tecnologias da World Wide Web, juntamente com HTML e CSS. A partir de 2022, aproximadamente 98% dos sites utilizam JavaScript no lado do cliente para controlar o comportamento das páginas da web, muitas vezes incorporando bibliotecas de terceiros.",
+    img: Js,
+  },
+  {
+    id: 4,
+    title: "React",
+    description: "O React.js é uma biblioteca JavaScript para construir interfaces de usuário interativas e reativas. Ele usa componentes reutilizáveis e oferece um desempenho rápido e escalável para desenvolvimento web moderno.",
+    img: ReactLogo,
+  },
+];
